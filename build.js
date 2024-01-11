5bcd787ba0f2a91ce9fbe738b5d460fafb999935
Brian Beard <brian.a.beard@gmail.com>
2024-01-11 15:17:15 -0600

    update to latest deps

--- build.js
+++ build.js
@@ -5,12 +5,22 @@ const options = { clean: true };
 
 async function build() {
   await generateFromFolder(
+    "node_modules/heroicons/16/solid",
+    "src/lib/micro",
+    options
+  );
+
+  let index = await fs.readFile("src/lib/micro/index.js", { encoding: "utf-8" });
+  index = index.replace(/export { (\w+) }/g, "export { $1 as $1Icon }").replace(/from ".\/(\w+)";/g, 'from "./$1/index.js";');
+  await fs.writeFile("src/lib/micro/index.js", index);
+
+  await generateFromFolder(
     "node_modules/heroicons/20/solid",
     "src/lib/mini",
     options
   );
 
-  let index = await fs.readFile("src/lib/mini/index.js", { encoding: "utf-8" });
+  index = await fs.readFile("src/lib/mini/index.js", { encoding: "utf-8" });
   index = index.replace(/export { (\w+) }/g, "export { $1 as $1Icon }").replace(/from ".\/(\w+)";/g, 'from "./$1/index.js";');
   await fs.writeFile("src/lib/mini/index.js", index);
 
