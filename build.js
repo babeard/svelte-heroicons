5f215e41eee1983ce3fad4be21437302be98c49a
Brian Beard <brian.a.beard@gmail.com>
2024-01-11 18:43:46 -0600

    fix package.json exports

--- build.js
+++ build.js
@@ -1,48 +1,56 @@
-import * as fs from "fs/promises";
-import { generateFromFolder } from "svg-to-svelte";
+import * as fs from 'fs/promises';
+import { generateFromFolder } from 'svg-to-svelte';
+import PackageJson from '@npmcli/package-json';
 
-const options = { clean: true };
+const options = {
+	clean: true
+};
+
+const exprts = {
+	'.': {
+		types: './dist/index.d.ts',
+		svelte: './dist/index.js'
+	},
+	'./micro': './dist/micro/index.js',
+	'./mini': './dist/mini/index.js',
+	'./outline': './dist/outline/index.js',
+	'./solid': './dist/solid/index.js'
+};
+
+async function generateType(sourceFolder, type) {
+	await generateFromFolder(`node_modules/heroicons/${sourceFolder}`, `src/lib/${type}`, {
+		...options,
+		onModuleName(name) {
+			exprts[`./${type}/${name}`] = `./dist/${type}/${name}/index.js`;
+			return name;
+		}
+	});
+
+	let index = await fs.readFile(`src/lib/${type}/index.js`, { encoding: 'utf-8' });
+	index = index
+		.replace(/export { (\w+) }/g, 'export { $1 as $1Icon }')
+		.replace(/from ".\/(\w+)";/g, 'from "./$1/index.js";');
+	await fs.writeFile(`src/lib/${type}/index.js`, index);
+}
+
+async function updatePackageJsonExports() {
+	const pkg = await PackageJson.load('./');
+
+	pkg.update({
+		exports: {
+			...exprts
+		}
+	});
+
+	await pkg.save();
+}
 
 async function build() {
-  await generateFromFolder(
-    "node_modules/heroicons/16/solid",
-    "src/lib/micro",
-    options
-  );
-
-  let index = await fs.readFile("src/lib/micro/index.js", { encoding: "utf-8" });
-  index = index.replace(/export { (\w+) }/g, "export { $1 as $1Icon }").replace(/from ".\/(\w+)";/g, 'from "./$1/index.js";');
-  await fs.writeFile("src/lib/micro/index.js", index);
-
-  await generateFromFolder(
-    "node_modules/heroicons/20/solid",
-    "src/lib/mini",
-    options
-  );
-
-  index = await fs.readFile("src/lib/mini/index.js", { encoding: "utf-8" });
-  index = index.replace(/export { (\w+) }/g, "export { $1 as $1Icon }").replace(/from ".\/(\w+)";/g, 'from "./$1/index.js";');
-  await fs.writeFile("src/lib/mini/index.js", index);
-
-  await generateFromFolder(
-    "node_modules/heroicons/24/solid",
-    "src/lib/solid",
-    options
-  );
-
-  index = await fs.readFile("src/lib/solid/index.js", { encoding: "utf-8" });
-  index = index.replace(/export { (\w+) }/g, "export { $1 as $1Icon }").replace(/from ".\/(\w+)";/g, 'from "./$1/index.js";');
-  await fs.writeFile("src/lib/solid/index.js", index);
-
-  await generateFromFolder(
-    "node_modules/heroicons/24/outline",
-    "src/lib/outline",
-    options
-  );
-
-  index = await fs.readFile("src/lib/outline/index.js", { encoding: "utf-8" });
-  index = index.replace(/export { (\w+) }/g, "export { $1 as $1Icon }").replace(/from ".\/(\w+)";/g, 'from "./$1/index.js";');
-  await fs.writeFile("src/lib/outline/index.js", index);
+	await generateType('16/solid', 'micro');
+	await generateType('20/solid', 'mini');
+	await generateType('24/solid', 'solid');
+	await generateType('24/outline', 'outline');
+	await updatePackageJsonExports();
 }
 
 build();
